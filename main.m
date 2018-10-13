602c1d0b417bcf2f5ac4e76e7b8d0b656c7f05ac
Leila Hashemi <leila_hashemi94@yahoo.com>
2018-10-13 05:29:09 +0330

    Pressure Distribution

--- main.m
+++ main.m
@@ -4,5 +4,10 @@ fileName = 'Berea';
 % Pressure difference
 inletPressure = 1;
 outletPressure = 0;
+sig_ow = 0.02; 
+water_viscosity = 0.001;
+oil_viscosity = 0.001;
+Pc = 3000;
 network = Network(fileName);
 fprintf('Porosity of the model is: %3.5f \n', network.calculatePorosity())
+
